f95017413b3db210321fa8e16785e91b367abb80
Prakhar256 <prakharjain256@gmail.com>
2024-03-27 19:21:29 +0530

    Edit profile feature added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,47 @@
-import { getAuth } from "firebase/auth";
+import { auth, getAuth, updateCurrentUser, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import {toast} from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail]=useState(false);
+
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
+
+  function onChange(e) {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [e.target.id]: e.target.value,
+      };
+    });
+  }
+  async function  onSubmit(){
+    try {
+      if(auth.currentUser.displayName !== name){
+        // update the name in firebase auth
+        await updateProfile(auth.currentUser,{
+          displayName : name,
+        });
+        // update the name in firebase auth
+        const docRef= doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef,{name});
+      }
+      toast.success("Your profile has been updated successfully!");
+    }  
+
+    catch (error) {
+      toast.error("Couldn't update the profile details");
+    }
+  }
   const { name, email } = formData;
   function onLogout() {
     auth.signOut();
@@ -30,6 +62,8 @@ export default function Profile() {
               type="text"
               id="name"
               value={name}
+              disabled={!changeDetail}
+              onChange={onChange}
               className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out 
               placeholder="Your Full Name"
               }`}
@@ -49,9 +83,14 @@ export default function Profile() {
               <p className="flex items-center ">
                 Do you want to change your name?
                 <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
                   className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-3 cursor-pointer"
                 >
-                  Edit
+                  {changeDetail ? "Apply change" : "Edit"}
+                  {/* Edit */}
                 </span>
               </p>
               <p
