1f3c6016bb008e0d3c3dc90292bdbf67840edbc7
Prakhar256 <prakharjain256@gmail.com>
2024-03-30 02:00:32 +0530

    Added contact landlord and live location functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,20 +8,24 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/bundle";
 import {
-    FaShare,
-    FaMapMarkerAlt,
-    FaBed,
-    FaBath,
-    FaParking,
-    FaChair,
-  } from "react-icons/fa";
+  FaShare,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
 import { getAuth } from "firebase/auth";
+import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Listing() {
+  const auth = getAuth();
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
   useEffect(() => {
     async function fetchListing() {
       const docRef = doc(db, "listings", params.listingId);
@@ -31,18 +35,14 @@ export default function Listing() {
         setLoading(false);
       }
     }
-    if (params.listingId) {
-      fetchListing();
-    }
+    fetchListing();
   }, [params.listingId]);
-
   if (loading) {
     return <Spinner />;
   }
-
   return (
     <main>
-         <Swiper
+      <Swiper
         slidesPerView={1}
         navigation
         pagination={{ type: "progressbar" }}
@@ -63,16 +63,16 @@ export default function Listing() {
         ))}
       </Swiper>
       <div
-        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"  
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
         onClick={() => {
-            navigator.clipboard.writeText(window.location.href);
-            // This would copy down the link to your clipboard!
-            setShareLinkCopied(true);
-            setTimeout(() => {
-                setShareLinkCopied(false);
-            }, 2000);
+          navigator.clipboard.writeText(window.location.href);
+          // This would copy down the link to your clipboard!
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
         }}
-        >
+      >
         <FaShare className="text-lg text-slate-500" />
       </div>
       {shareLinkCopied && (
@@ -80,57 +80,91 @@ export default function Listing() {
           Link Copied
         </p>
       )}
+
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
         <div className=" w-full ">
-            <p className="text-2xl font-bold mb-3 text-blue-900">
-                {listing.name} - ${" "}
-                {listing.offer
-                ? listing.discountedPrice
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                : listing.regularPrice
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                {listing.type === "rent" ? " / month" : ""}
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - Rs.{" "}
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? " / month" : ""}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "Rent" : "Sale"}
             </p>
-            <p className="flex items-center mt-6 mb-3 font-semibold">
-                <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.offer && (
+              <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+                Rs.{+listing.regularPrice - +listing.discountedPrice} discount
+              </p>
+            )}
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
+            <li className="flex items-center whitespace-nowrap">
+              <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaParking className="text-lg mr-1" />
+              {listing.parking ? "Parking spot" : "No parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaChair className="text-lg mr-1" />
+              {listing.furnished ? "Furnished" : "Not furnished"}
+            </li>
+          </ul>
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
+              >
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
+        </div>
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lon]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lon]}
+            >
+              <Popup>
                 {listing.address}
-            </p>
-            <div className="flex justify-start items-center space-x-4 w-[75%]">
-                <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
-                {listing.type === "rent" ? "Rent" : "Sale"}
-                </p>
-                {listing.offer && (
-                <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
-                    ${+listing.regularPrice - +listing.discountedPrice} discount
-                </p>
-                )}
-            </div>
-            <p className="mt-3 mb-3">
-                <span className="font-semibold">Description - </span>
-                {listing.description}
-            </p>
-            <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
-                <li className="flex items-center whitespace-nowrap">
-                    <FaBed className="text-lg mr-1" />
-                    {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
-                </li>
-                <li className="flex items-center whitespace-nowrap">
-                    <FaBath className="text-lg mr-1" />
-                    {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
-                </li>
-                <li className="flex items-center whitespace-nowrap">
-                    <FaParking className="text-lg mr-1" />
-                    {listing.parking ? "Parking spot" : "No parking"}
-                </li>
-                <li className="flex items-center whitespace-nowrap">
-                    <FaChair className="text-lg mr-1" />
-                    {listing.furnished ? "Furnished" : "Not furnished"}
-                </li>
-            </ul>
-            </div>
-        </div> 
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
     </main>
   );
 }
